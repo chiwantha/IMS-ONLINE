16e1ba859497062ec2c7f6747737b773b6ea7014
Kasun Chiwantha <kasunchiwantha789@gmail.com>
2024-08-04 00:37:19 +0530

    made some privacy changes on configurations

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 
-// $conn = mysqli_connect('153.92.15.9', 'u674260424_Sasa', 'Kchordgroup*789789','u674260424_ims') or die('connection failed');
+// $conn = mysqli_connect('153.92.15.910', 'u674260424_Sasa', '','u674260424_ims') or die('connection failed');
 // $conn = mysqli_connect('localhost', 'root', 'root','ims') or die('connection failed');
-$conn = mysqli_connect('153.92.15.9', 'u674260424_ims', 'Kchordgroup*789789','u674260424_ims') or die('connection failed');
+$conn = mysqli_connect('153.92.15.910', 'u674260424_ims', '','u674260424_ims') or die('connection failed');
 
 ?>
